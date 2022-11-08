36508cb190c45b423d1ff995ceae0e182a755804
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2022-11-08 07:53:59 +0700

    origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://giaihung-booking-app.com',
+  'https://giaihung-booking-app.netlify.app',
   'http://127.0.0.1:5500',
   'http://127.0.0.1:5173',
   'https://google.com',
